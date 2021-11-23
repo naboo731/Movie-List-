3e1a4e9e3f3c530ceabe07133be3d8b8e67c3e3e
Nichole Boothman <nbooth731@gmail.com>
2021-11-23 16:00:49 -0500

    Step 2 Complete

---
+++ movielist.js
@@ -0,0 +1,15 @@
+function addMovie(event) {
+    event.preventDefault()
+    const inputField = document.querySelector('input')
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value 
+    movie.appendChild(movieTitle)
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ''
+}
+
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
+
